ccb25b2740f8977716f3fece1a568c674ce004cd
SouleymaneFOMBA <babalarab2012@gmail.com>
2023-10-30 23:07:14 +0000

    firs commit

---
+++ src/main/java/com/microfinance/agroInvest/AgroInvestApplication.java
@@ -0,0 +1,14 @@
+package com.microfinance.agroInvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroInvestApplication.class, args);
+        System.out.println("Hello word");
+	}
+
+}
---
+++ src/test/java/com/microfinance/agroInvest/AgroInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microfinance.agroInvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
